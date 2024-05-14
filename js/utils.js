0623adec2f8688168eb9379158cfc6fe12f93f0e
Heera  Nand <heera.nand@A006-01422.local>
2024-05-14 18:36:29 +0500

    Added collision detection function and helper, necessary for game mechanics.

--- js/utils.js
+++ js/utils.js
@@ -19,3 +19,16 @@ const loadImageHelper = (imageName) => {
 const loadImage = async (imageName) => {
     return await loadImageHelper(imageName);
 }
+
+const detectCollision = (node1, node2) => {
+    return haveIntersection(node1.getClientRect(), node2.getClientRect());
+}
+
+const haveIntersection = (r1, r2) => {
+    return !(
+        r2.x > r1.x + r1.width ||
+        r2.x + r2.width < r1.x ||
+        r2.y > r1.y + r1.height ||
+        r2.y + r2.height < r1.y
+    );
+}
